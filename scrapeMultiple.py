096fa4bb23f0daa7370ab268a1209be6ee325a73
MilosGlamocak <milosglamocak@gmail.com>
2024-08-15 13:28:38 +0200

    mintict pao

--- scrapeMultiple.py
+++ scrapeMultiple.py
@@ -4,7 +4,8 @@ def run_scrapy_spiders():
     commands = [
         'cd technoshop && scrapy crawl technoshopSpider -O technoshopItems.json',
         'cd threedbox && scrapy crawl a3dboxSpider -O threedboxItems.json',
-        'cd deltapc && scrapy crawl deltapcSpider -O deltapcItems.json'
+        'cd deltapc && scrapy crawl deltapcSpider -O deltapcItems.json',
+        'cd mintict && scrapy crawl mintictSpider -O mintictItems.json',
     ]
 
     for command in commands:
